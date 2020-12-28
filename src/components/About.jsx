2c861419015633920dd9a3b5e0f25a2ec05196bc
unknown <tom_aldren@hotmail.comgit config --global user.name Tom>
2020-12-28 12:40:00 +0000

    background-color & padding changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,47 +1,52 @@
 import React from "react";
-import HeadsetIcon from "@material-ui/icons/Headset";
 
 function About(props) {
   return (
-    <div className="main">
+    <div className = "main" >
+      <div className = "main-container" >
+        <h1 className = "main-title" > about < /h1>
+        <p className = "body-text about-comment" >
+        Traction Music is run by Thomas Aldren, a London-based freelance musician.
+        </p>
+      </div>
+      <div className="fade-background">
+      <hr className="dotted-line" />
       <div className="main-container">
-        <h1 className="main-title">about</h1>
-        <p className="body-text about-comment">
-          Traction Music is run by Thomas Aldren, a London-based freelance
-          musician.
-        </p>
-        <p className="icon">
-          <HeadsetIcon className="headphone-icon" fontSize="large" />
-        </p>
-        <p className="body-text about-text">
-          Tom graduated from King's College London with 1st Class Hons in 2014,
-          with a particular focus on techniques of composition. Following
-          master's degrees in performance in both London and Munich, he has
-          combined a career as a concert violinist with providing arrangements
-          for a wide range of clients, which have been performed in concerts and
-          festivals across the UK and Europe. <br />
-          Performing highlights include concerts as co-leader of the BBC
-          Symphony and Bergen Philharmonic Orchestras, as Principal Second
-          Violin of the Bournemouth Symphony Orchestra, and chamber music
-          performances live on Radio 3 and in major concert halls in the UK and
-          Europe. He has also performed with artists such as Burt Bacharach,
-          Quincy Jones, Father John Misty and Freddie McGregor in venues from
-          Ronnie Scott's and Hammersmith Apollo to Glastonbury. His experience
-          across a huge range of musical genres gives him huge versatility when
-          it comes to arranging music, and his portfolio encompasses an eclectic
-          mix of styles and instrumentation.
-        </p>
-        <p className="icon">
-          <HeadsetIcon className="headphone-icon" fontSize="large" />
-        </p>
-        <h2 className="home-contact-container about-contact home-subtitle">
-          For more information,
-          <a href="mailto:info@tractionmusic.co.uk" className="home-contact-link">
-            {" "}
-            contact us{" "}
-          </a>
-          with your enquiry
-        </h2>
+      <p className = "body-text about-text" >
+      Tom graduated from King's College London with 1st Class Hons in 2014,
+      with a particular focus on techniques of composition. Following master's
+      degrees in performance in both London and Munich, he has
+      combined a career as a concert violinist with providing arrangements
+      for a wide range of clients, which have been performed in concerts and
+      festivals across the UK and Europe. His arrangement portfolio encompasses
+      an eclectic mix of styles and instrumentation, from solo string instruments
+      to large scale ensembles. His focus is primarily on using the available forces
+      to their full capacity in order to bring new life to the original material.
+      <br />
+      Tom's arrangements are influenced by his performing career, which is hugely varied.
+      Performing highlights include concerts as co-leader of the BBC Symphony and
+      Bergen Philharmonic Orchestras, as Principal Second Violin of the Bournemouth
+      Symphony Orchestra, and chamber music performances live on Radio 3 and in major
+      concert halls in the UK and Europe. He has also performed with artists such as
+      Burt Bacharach, Quincy Jones, Father John Misty and Freddie McGregor in venues
+      from Ronnie Scott's and Hammersmith Apollo to Glastonbury. This intimate experience
+      of such a wide array of musical genres is invaluable when it comes to composition and
+      arranging music.
+      </p>
+      </div>
+      <hr className="dotted-line" />
+      </div>
+      <div className="main-container">
+      <h2 className = "about-contact home-contact-container" >
+      For more information,
+      <a href = "mailto:info@tractionmusic.co.uk"
+      className = "home-contact-link" >
+      {" "}
+      contact us
+      {" "}
+      </a>
+      with your enquiry
+      </h2>
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home(props) {
       >
         <Grid item sm={12} md={6}>
           <h1 className="main-title">
-            We provide musical arrangements and recordings to meet your
+            we provide musical arrangements and recordings to meet your
             requirements.
           </h1>
         </Grid>
@@ -24,48 +24,50 @@ function Home(props) {
           we've got you covered each step of the way.
         </p>
       </div>
-      <hr className="dotted-line" />
-      <div className="main-container">
-        <h2 className="home-subtitle">Our bespoke service includes:</h2>
-        <ul className="body-text home-list">
-          <li>
-            Composition of entirely new music, or additional instrumental lines
-            for completed pieces
-          </li>
-          <li className="icon">
-            <HeadsetIcon className="headphone-icon" fontSize="large" />
-          </li>
-          <li>
-            Vibrant and innovative arrangements for any ensemble, from solo
-            instruments to full orchestra and any combination in between
-          </li>
-          <li className="icon">
-            <HeadsetIcon className="headphone-icon" fontSize="large" />
-          </li>
-          <li>
-            Recording of instrumental tracks by top London-based session
-            musicians
-          </li>
-          <li className="icon">
-            <HeadsetIcon className="headphone-icon" fontSize="large" />
-          </li>
-          <li>
-            Post-production as required - we can send raw recorded material as
-            separate tracks, or mixed and packaged as a single audio file
-          </li>
-          <li className="icon">
-            <HeadsetIcon className="headphone-icon" fontSize="large" />
-          </li>
-          <li>
-            In order to distribute an arrangement or recording for commercial
-            use, the copyright of the original musical work must be respected.
-            We will contact the copyright owner as required and arrange for
-            permission of any arrangements we make. Any related fees will be
-            added to our quote
-          </li>
-        </ul>
+      <div className="fade-background">
+        <hr className="dotted-line" />
+        <div className="main-container">
+          <h2 className="home-subtitle">Our bespoke service includes:</h2>
+          <ul className="body-text home-list">
+            <li>
+              Composition of entirely new music, or additional instrumental lines
+              for completed pieces
+            </li>
+            <li className="icon">
+              <HeadsetIcon className="headphone-icon" fontSize="large" />
+            </li>
+            <li>
+              Vibrant and innovative arrangements for any ensemble, from solo
+              instruments to full orchestra and any combination in between
+            </li>
+            <li className="icon">
+              <HeadsetIcon className="headphone-icon" fontSize="large" />
+            </li>
+            <li>
+              Recording of instrumental tracks by top London-based session
+              musicians
+            </li>
+            <li className="icon">
+              <HeadsetIcon className="headphone-icon" fontSize="large" />
+            </li>
+            <li>
+              Post-production as required - we can send raw recorded material as
+              separate tracks, or mixed and packaged as a single audio file
+            </li>
+            <li className="icon">
+              <HeadsetIcon className="headphone-icon" fontSize="large" />
+            </li>
+            <li>
+              In order to distribute an arrangement or recording for commercial
+              use, the copyright of the original musical work must be respected.
+              We will contact the copyright owner as required and arrange for
+              permission of any arrangements we make. Any related fees will be
+              added to our quote
+            </li>
+          </ul>
+        </div>
+        <hr className="dotted-line" />
       </div>
-      <hr className="dotted-line" />
       <div className="main-container">
         <p className="home-comment body-text">
           Every step is completed in-house and comes as a package, cutting out
